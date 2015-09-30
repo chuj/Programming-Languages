c97d99e441fa714d0b5b3153d33c31197b2c6e09
chuj <jonathanchu0205@gmail.com>
2015-09-30 13:18:14 -0700

    Finish up to fixed point problem

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -23,7 +23,31 @@ let equalSets a b =
 
 (* PROBLEM 3: SET UNION *)
 
+(* Returns the union of two sets, repeating elements is OK *)
 let rec setUnion a b = match b with
   |[] -> a (* if b is empty set, then the union is a *)
-  |first::rest -> setUnion (a @ [first]) rest
+  |first::rest -> setUnion (a @ [first]) rest;;
+
+(* PROBLEM 4: SET INTERSECTION *)
+
+(* Returns the intersection of two sets *)
+let rec setIntersection a b = match b with
+  |[] -> []
+  |first::rest -> if isElement first a then first :: (setIntersection a rest)
+                  else setIntersection a rest;;
+
+(* PROBLEM 5: SET DIFF *)
+
+(* returns a list representing a−b, that is,
+ the set of all members of a that are not also members of b *)
+let rec setDiff a b = match a with
+  |[] -> []
+  |first::rest -> if isElement first b then setDiff rest b
+                  else first :: (setDiff rest b);;
+
+(* PROBLEM 5: FIXED POINTS *)
+let rec computed_fixed_point eq f x = 
+  if eq (f x) x then x
+  else computed_fixed_point eq f (f x);;
+
 
