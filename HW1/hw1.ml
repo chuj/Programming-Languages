ce5eba6be7aedbe979eb68826a2df190e0234078
chuj <jonathanchu0205@gmail.com>
2015-09-29 19:30:02 -0700

    Finish probs 1-3

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -12,4 +12,18 @@ let rec isElement element set = match set with
   |first::rest -> if (first = element) then true
                   else isElement element rest;;
   
-(* PROBLEM 2: SUBSET*)
+(* PROBLEM 2: EQUAL SETS*)
+
+(* Returns true if the two sets are equal, repeating elements is OK *)
+let equalSets a b =
+  if subset a b then 
+    if subset b a then true
+    else false
+  else false;;
+
+(* PROBLEM 3: SET UNION *)
+
+let rec setUnion a b = match b with
+  |[] -> a (* if b is empty set, then the union is a *)
+  |first::rest -> setUnion (a @ [first]) rest
+
