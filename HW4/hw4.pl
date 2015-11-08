272950c053656d9f53269143be1a11f3c0b87999
chuj <jonathanchu0205@gmail.com>
2015-11-07 17:00:20 -0800

    Finishes part 1

--- HW4/hw4.pl
+++ HW4/hw4.pl
@@ -2,35 +2,10 @@
 % C, a list of numeric cage constraints as described below.
 % T, a list of list of integers. All the lists have length N. This represents the N×N grid.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%      PART 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-kenken(N, C, T) :- 
-  % use length/2 to see if length of T is same as N
-  length(T, N),
-  % additionally, each list in T must also be length N
-  maplist(getlength(N) , T),
-  % Constraints must be met
-  maplist(iterate_constraints(T) , C)
-  % no numbers can be repeated in row or columns
-  maplist(fd_all_different, T),
-  % transpose T, call transposed matrix X
-  transpose(T, X),
-  % no numbers can be repeated in row or columns
-  maplist(fd_all_different, X),
-
-  .
-  
-%helper goal for matching constraints
-iterate_constraints(T, Constraint) :- match_constraint(T, Constraint).
-%match the constraint with the following forms:
-match_constraint(T, +(S, L)) :- addition( L, 0, S, T) .
-match_constraint(T, *(P, L)) :- multiplication(L, 1, P, T).
-match_constraint(T, -(D, J, K)) :- subtraction(D, J, K, T).
-match_constraint(T, /(Q, J, K)) :- division(Q, J, K, T).
-
-
-
-% helper goal for checking list length equals N
-getlength(N, List) :-  length(List, N). 
 
 % Addition rules
 % Base Case: add [] to S gives you S
@@ -42,14 +17,8 @@ addition([First|Rest], RunningSum , S , T) :-
   addition(Rest, NewRunSum, S, T)
 .
 
-%Helper goal to get element value in matrix
-getElement(Row-Col, V, T) :-
-  nth( Row, T, T_row),
-  nth( Col, T_row, V)  
-.
-
 % Subtraction rules
-subtraction(D, J, K, _, D_result).
+subtraction(D, _, _, _, D).
 %Recursive Subtraction - case j - k
 subtraction(D, J, K, T) :-
   getElement(J, J_val, T),
@@ -76,21 +45,33 @@ multiplication([First | Rest], RunningProd, P, T) :-
 
 % Division rules
 %Base case
-division(Q, J, K, _, Q).
+division(Q, _, _, _, Q).
 %Recursive Division case j / k
-division(Q, J, K, T, Q_result) :- 
+division(Q, J, K, T) :- 
   getElement(J, J_val, T),
   getElement(K, K_val, T),
   Q_result #= J_val / K_val,
   division(Q, J, K, T, Q_result)
 .
 %Recursive Division case k / j
-division(Q, J, K, T, Q_result) :- 
+division(Q, J, K, T) :- 
   getElement(J, J_val, T),
   getElement(K, K_val, T),
   Q_result #= K_val / J_val,
   division(Q, J, K, T, Q_result)
 .
+
+%Helper goal to get element value in matrix
+getElement(Row-Col, V, T) :-
+  nth( Row, T, T_row),
+  nth( Col, T_row, V)  
+.
+
+% helper goal for checking list length equals N
+getlength(N, List) :-  length(List, N). 
+
+% Use fd_domain to set values from 1 to N
+setDomain(N, List) :- fd_domain(List, 1, N).
 
 % Transposing a list of lists, from clpfd.pl module
 transpose([], []).
@@ -105,5 +86,59 @@ transpose([_|Rs], Ms, [Ts|Tss]) :-
 lists_firsts_rests([], [], []).
 lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
         lists_firsts_rests(Rest, Fs, Oss).
+
+kenken(N, C, T) :- 
+  % use length/2 to see if length of T is same as N
+  length(T, N),
+  % additionally, each list in T must also be length N
+  maplist(getlength(N) , T),
+  % set the domain over all the elements
+  maplist(setDomain(N), T),
+  % Constraints must be met
+  maplist(iterate_constraints(T) , C),
+  % no numbers can be repeated in row or columns
+  maplist(fd_all_different, T),
+  % transpose T, call transposed matrix X
+  transpose(T, X),
+  % no numbers can be repeated in row or columns
+  maplist(fd_all_different, X),
+  % use fd_labeling to assign values
+  maplist(fd_labeling, T)
+  .
+
+%helper goal for matching constraints
+iterate_constraints(T, Constraint) :- match_constraint(T, Constraint).
+%match the constraint with the following forms:
+match_constraint(T, +(S, L)) :- addition( L, 0, S, T) .
+match_constraint(T, *(P, L)) :- multiplication(L, 1, P, T).
+match_constraint(T, -(D, J, K)) :- subtraction(D, J, K, T).
+match_constraint(T, /(Q, J, K)) :- division(Q, J, K, T).
+
+kenken_testcase(
+  6,
+  [
+   +(11, [1-1, 2-1]),
+   /(2, 1-2, 1-3),
+   *(20, [1-4, 2-4]),
+   *(6, [1-5, 1-6, 2-6, 3-6]),
+   -(3, 2-2, 2-3),
+   /(3, 2-5, 3-5),
+   *(240, [3-1, 3-2, 4-1, 4-2]),
+   *(6, [3-3, 3-4]),
+   *(6, [4-3, 5-3]),
+   +(7, [4-4, 5-4, 5-5]),
+   *(30, [4-5, 4-6]),
+   *(6, [5-1, 5-2]),
+   +(9, [5-6, 6-6]),
+   +(8, [6-1, 6-2, 6-3]),
+   /(2, 6-4, 6-5)
+  ]
+).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%      PART 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 
 
